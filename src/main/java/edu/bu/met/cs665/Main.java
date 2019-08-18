8dd6fbbb586697d8b57afc5acbbe2e3202979699
Susie Kim <susiekim21@gmail.com>
2019-08-17 21:14:32 -0400

    Clearing old code for starting project

--- src/main/java/edu/bu/met/cs665/Main.java
+++ src/main/java/edu/bu/met/cs665/Main.java
@@ -8,32 +8,6 @@ public class Main {
    * @param args not used
    */
   public static void main(String[] args) {
-    BeverageVendingMachine beverageVendingMachine = new BeverageVendingMachine();
-    beverageVendingMachine.initialize();
 
-    beverageVendingMachine.setBeveragePrice("Americano", 1);
-    beverageVendingMachine.setBeveragePrice("Espresso", 3);
-    beverageVendingMachine.setBeveragePrice("Latte Macchiato", 1);
-    beverageVendingMachine.setBeveragePrice("Black Tea", 2);
-    beverageVendingMachine.setBeveragePrice("Yellow Tea", 2);
-    beverageVendingMachine.setBeveragePrice("Green Tea", 1);
-
-    beverageVendingMachine.brew("Latte Macchiato");
-    beverageVendingMachine.serve();
-
-    beverageVendingMachine.brew("Espresso");
-    beverageVendingMachine.serve();
-
-    beverageVendingMachine.brew("Americano");
-    beverageVendingMachine.serve();
-
-    beverageVendingMachine.brew("Black Tea");
-    beverageVendingMachine.serve();
-
-    beverageVendingMachine.brew("Green Tea");
-    beverageVendingMachine.serve();
-
-    beverageVendingMachine.brew("Yellow Tea");
-    beverageVendingMachine.serve();
   }
 }
--- src/test/java/edu/bu/met/cs665/AllTests.java
+++ src/test/java/edu/bu/met/cs665/AllTests.java
@@ -1,19 +1,12 @@
 package edu.bu.met.cs665;
 
-import edu.bu.met.cs665.state.ReadyMachineStateTest;
-import edu.bu.met.cs665.state.UninitializedMachineStateTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-    BeveragePricesTest.class,
-    MachineBrewTest.class,
-    MachineMachineStateTest.class,
-    ReadyMachineStateTest.class,
-    UninitializedMachineStateTest.class,
-    BeverageInventoryTest.class
+
 })
 public class AllTests {
 
