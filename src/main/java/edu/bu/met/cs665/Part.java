aa61809fe938a5c3ee27591cf9d2cd364d78d0a8
Susie Kim <susiekim21@gmail.com>
2019-08-21 02:52:47 -0400

    adding part doc

--- src/main/java/edu/bu/met/cs665/Part.java
+++ src/main/java/edu/bu/met/cs665/Part.java
@@ -4,14 +4,17 @@ import java.time.LocalDate;
 
 public interface Part {
 
+  // Lifespan is # of years a part is expected to last before undergoing required maintenance
   void setLifespan(int years);
 
   int getLifespan();
 
+  // Start date is the date the part is installed and initiated
   void setStartDate(LocalDate date);
 
   LocalDate getStartDate();
 
+  // Buffer is the # of years until maintenance that warning should be triggered
   void setBuffer(int buffer);
 
   int getBuffer();
@@ -20,7 +23,9 @@ public interface Part {
 
   String getName();
 
+  // Retrieves health status of part (healthy/warning/critical)
   String checkHealth(LocalDate date);
 
+  // Retrieves # of years left until required maintenance 
   int getYearsLeft(LocalDate date);
 }
