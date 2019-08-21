002f86172e5112c95dfe816846238f747d0c3daa
Susie Kim <susiekim21@gmail.com>
2019-08-21 02:49:23 -0400

    Adding doc for aircraft state

--- src/main/java/edu/bu/met/cs665/AircraftState.java
+++ src/main/java/edu/bu/met/cs665/AircraftState.java
@@ -2,13 +2,16 @@ package edu.bu.met.cs665;
 
 public interface AircraftState {
 
-  // Assesses
+  // Assesses status of aircraft based on part inventory's maintenance status
   void evaluate();
 
+  // Prints evaluation results from assessment
   void printResults();
 
+  // Performs required maintenance based on critical/warning parts
   void performMaintenance();
 
+  // Retrieves status name
   String get();
 
 }
